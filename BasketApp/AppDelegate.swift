a287ffb292b78c25d5e1267ed9e5eec90b1fedec
Даниил <d.v.shestopalov@mail.ru>
2021-04-16 09:15:20 +0300

    test comit

--- BasketApp/AppDelegate.swift
+++ BasketApp/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-// comment for commit55
+// comment for commit56
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if myTeamRead.count == 0
